ace2bff1a07ce838abababa508a00f56bb9efc06
Sonu Kumar <sonucse103@gmail.com>
2026-02-13 00:11:57 +0530

    create project bg chnager

---
+++ 4bgchanger/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+
+function App() {
+  const [color, setColor] = useState("olive");
+
+  return (
+    <div
+      className="w-full h-screen duration-200"
+      style={{ backgroundColor: color }}
+    >
+      <div className="fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg px-3 py-2 rounded-3xl">
+          <button
+            className="outline-none px-4 py-1 rounded-lg"
+            style={{ backgroundColor: "red" }}
+            onClick={() => setColor("red")}
+          >
+            red
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-lg"
+            style={{ backgroundColor: "green" }}
+            onClick={()=>setColor("green")}
+          >
+            green
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-lg"
+            style={{ backgroundColor: "yellow" }}
+            onClick={()=>setColor("yellow")}
+          >
+            yellow
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-lg"
+            style={{ backgroundColor: "orange" }}
+            onClick={()=>setColor("orange")}
+          >
+            orange
+          </button>
+          <button
+            className="outline-none px-4 py-1 rounded-lg"
+            style={{ backgroundColor: "gray" }}
+            onClick={()=>setColor("gray")}
+          >
+            gray
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
