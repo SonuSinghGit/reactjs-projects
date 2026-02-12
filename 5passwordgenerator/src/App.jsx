4e3e46b9e83fc8ff9af348241be6700d750f09c3
Sonu Kumar <sonucse103@gmail.com>
2026-02-13 00:18:04 +0530

    Creat password generator app

---
+++ 5passwordgenerator/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='w-full h-screen text-3xl'>
+      hello react
+    </div>
+  )
+}
+
+export default App
