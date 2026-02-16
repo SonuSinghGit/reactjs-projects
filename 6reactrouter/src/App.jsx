292f2ed3ed259a194299dd2516c6648077f7c963
Sonu Kumar <sonucse103@gmail.com>
2026-02-17 00:09:27 +0530

    creaet project with react router dom

---
+++ 6reactrouter/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='w-full bg-gray-500 h-screen'>
+     <div className='text-3xl'>hello App file</div>
+    </div>
+  )
+}
+
+export default App
---
+++ 6reactrouter/src/components/footer/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+      fotter file
+    </div>
+  )
+}
+
+export default Footer
