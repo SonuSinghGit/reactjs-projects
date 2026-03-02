eb6cb2fc28e57e19a2fdeb2d06580c511ef732ac
Sonu Kumar <sonucse103@gmail.com>
2026-03-03 00:24:18 +0530

    Practise todo app

--- 8todowithcontext/src/App.jsx
+++ 8todowithcontext/src/App.jsx
@@ -1,5 +1,51 @@
+import React, { useState } from "react";
+
 function App() {
-  return <div className="text-center">Todo App</div>;
+  const [todo, setTodo] = useState([]);
+  const [input, setInput] = useState("");
+
+  const handleSubmit = () => {
+    setTodo([...todo, input]);
+    setInput("");
+  };
+
+  const handleDelete = (index) => {
+    const updatedTodo = todo.filter((_, i) => i !== index);
+    setTodo(updatedTodo);
+  };
+
+  return (
+    <div className="text-center">
+      <h1 className="text-center mt-2 text-3xl"> TODO APP </h1>
+      <div className="px-2">
+        <input
+          type="text"
+          placeholder="Enter your todo"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button type="submit" onClick={handleSubmit}>
+          Submit
+        </button>
+
+        {todo.map((todo, index) => {
+          return (
+            <ul key={index}>
+              <div className="flex justify-center gap-4">
+                <li>{todo}</li>
+                <button
+                  className="text-red-400"
+                  onClick={() => handleDelete(index)}
+                >
+                  delete
+                </button>
+              </div>
+            </ul>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
 
 export default App;
