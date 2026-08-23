6df8d2fbd13cd75728e0c2e2fc0eb93f557e8000
Sonu Kumar <sonucse103@gmail.com>
2026-08-23 23:54:23 +0530

    fetch data through useeffect

--- HUXN/1useeffect/src/App.jsx
+++ HUXN/1useeffect/src/App.jsx
@@ -1,11 +1,29 @@
-import React from 'react'
+import React from "react";
+import { useState, useEffect } from "react";
 
 const App = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts",
+      );
+      const data = await response.json();
+      if (data && data.length) setData(data);
+      console.log(data);
+    }
+    fetchData();
+  }, []);
+
   return (
     <div>
       <h1>Learn useEffect</h1>
+      {data.map((items) => (
+        <li>{items.title}</li>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
