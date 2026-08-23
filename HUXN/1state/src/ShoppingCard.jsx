55d157c96af3955914a102a17299adef12bd8e07
Sonu Kumar <sonucse103@gmail.com>
2026-08-23 22:46:16 +0530

    Add the feature to add list items

--- HUXN/1state/src/ShoppingCard.jsx
+++ HUXN/1state/src/ShoppingCard.jsx
@@ -1,11 +1,54 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function ShoppingCard() {
+  const [name, setName] = useState();
+  const [quantity, setQuantity] = useState();
+  const [items, setItems] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name, quantity)
+    if(!name || !quantity){
+        alert("enter name or quantity")
+        return;
+    }
+    const newItems= {
+        name,
+        quantity:parseInt(quantity)
+    }
+    setItems((preItems)=> [...preItems,newItems])
+    setName("");
+    setQuantity("");
+  };
+
   return (
     <div>
-      
+      <h1>Project 3 - Add Shopping card</h1>
+      <div>
+        <form onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="enter itms name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <input
+            type="number"
+            placeholder="enter quantity"
+            value={quantity}
+            onChange={(e) => setQuantity(e.target.value)}
+          />
+
+          <button type="submit">Add Items</button>
+        </form>
+        {items.map((items,index) => (
+          <ul key={index}>
+            <h2>Items: {items.name} "Qty:" {items.quantity}  </h2>
+          </ul>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default ShoppingCard
+export default ShoppingCard;
--- HUXN/1state/src/main.jsx
+++ HUXN/1state/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import ShoppingCard from './ShoppingCard.jsx'
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <ShoppingCard/>
   </React.StrictMode>,
 )
