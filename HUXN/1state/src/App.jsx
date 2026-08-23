1d550374f3311cab7e90e4543f25d08a2ff3e7f5
Sonu Kumar <sonucse103@gmail.com>
2026-08-23 22:11:01 +0530

    add the shopping card profile

--- HUXN/1state/src/App.jsx
+++ HUXN/1state/src/App.jsx
@@ -1,15 +1,26 @@
-import React from 'react'
-import { useState } from 'react'
+import React from "react";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [friends, setFriends] = useState(["Aman", "Sonu", "Ankit", "Ajay"]);
 
-  const [count,setCount] = useState(0);
-  
-  const handleIncrease = ()=>{
-    setCount(count+1);
+  const handleIncrease = () => {
+    setCount(count + 1);
+  };
+  const handleDecrease = () => {
+    setCount(count - 1);
+  };
+
+  const addFriend = ()=>{
+    setFriends([...friends,"Amit Shinha"])
   }
-  const handleDecrease = ()=>{
-    setCount(count-1);
+
+  const removeFriend = ()=>{
+    setFriends(friends.filter((name)=>name !== "Aman"));
+  }
+  const updateFriend = ()=>{
+    setFriends(friends.map((name)=>name === "Sonu" ? "Sunny Sharma":name));
   }
 
   return (
@@ -19,9 +30,15 @@ function App() {
       <button onClick={handleIncrease}>Increase +</button>
       <button onClick={handleDecrease}>Decrease -</button>
 
-
+      <h1>project 2 </h1>
+      {friends.map((name) => (
+        <li>{name}</li>
+      ))}
+      <button onClick={addFriend}>Add Friend</button>
+      <button onClick={removeFriend}>Remove Friend</button>
+      <button onClick={updateFriend}>Update Friend</button>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
